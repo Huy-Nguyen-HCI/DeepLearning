9676962eff10bdab011088b8b63a6d4a9c70fe35
HuyLafa <anhhuy0706@gmail.com>
2016-11-12 18:23:04 -0500

    Finish feedforward for fully connected layer.

--- FullyConnectedLayer.java
+++ FullyConnectedLayer.java
@@ -5,34 +5,60 @@ import Jama.Matrix;
 public class FullyConnectedLayer extends Layer {
 
 	int neuronCount;
-	Matrix[][] weights;
+	double[][] weights;
+	double[] oneDimensionalInput;
 
 	public FullyConnectedLayer( int neuronCount, int activationFunction ) {
 		super( activationFunction );
 		this.neuronCount = neuronCount;
 	}
 
-//	@Override
-//	public void setInput( Matrix[] input ) {
-//		this.input = input;
-//		// initialize weight array
-//		int inputSize = input[0].getRowDimension();
-//		weights = new double[neuronCount][][][];
-//		for ( int i = 0 ; i < weights.length ; i++ ) {
-//			weights[i] = new double[input.length][][];
-//			for ( int j = 0 ; j < weights[i].length ; j++ ) {
-//				weights[i][j] = new Matrix( inputSize, inputSize );
-//			}
-//		}
-//	}
-
-
-	public void computeLinearCombinations() {
-
+	public void setInput( Matrix[] input ) {
+		this.input = input;
+		int totalInputNeuron = input.length * input[0].getRowDimension() * input[0].getColumnDimension();
+		weights = new double[neuronCount][totalInputNeuron];
 	}
 
 
-//	public double output() {
-//
-//	}
+	public void setInput( double[] input ) {
+		oneDimensionalInput = input;
+		weights = new double[neuronCount][input.length];
+	}
+
+
+	public double[] getOutput() {
+		double[] output = new double[neuronCount];
+
+		// if input is a 3D matrix
+		if ( input != null ) {
+			double linearCombination = 0;
+			for ( int i = 0 ; i < output.length ; i++ ) {
+				output[i] = getOutputAtNeuron(i);
+			}
+			return output;
+		}
+
+		assert( oneDimensionalInput != null );
+		for ( int i = 0 ; i < output.length ; i++ ) {
+			for ( int j = 0 ; j < oneDimensionalInput.length ; j++ ) {
+				output[i] += oneDimensionalInput[i] * weights[i][j];
+			}
+		}
+		return output;
+	}
+
+
+	public double getOutputAtNeuron( int neuronIndex ) {
+		double output = 0;
+		int count = 0;
+		for ( int k = 0 ; k < input.length ; k++ ) {
+			for ( int i = 0 ; i < input[k].getRowDimension() ; i++ ) {
+				for ( int j = 0 ; j < input[k].getColumnDimension() ; j++ ) {
+					output += weights[neuronIndex][count] * input[k].get( i, j );
+					count ++;
+				}
+			}
+		}
+		return output;
+	}
 }
